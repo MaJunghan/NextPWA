d141420084adc64436bb6a4e9183352d6f02f9b1
MaJunghan <wjdgksak@naver.com>
2022-09-26 23:13:26 +0900

    next.js PWA connect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+    // [
+    //   typescript,
+    //   {
+    //     typescriptLoaderOptions: {
+    //       transpileOnly: false,
+    //     },
+    //   },
+    // ],
+  ],
+  nextConfig
+);
